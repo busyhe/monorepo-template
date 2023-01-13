f80e73b84fd0e6aea44a982d8ba8f069198824f8
busyhe <busyhe@qq.com>
2023-01-13 21:22:09 +0800

    feat(app): add antd

--- packages/app/src/main.tsx
+++ packages/app/src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ConfigProvider } from 'antd';
 import App from './App'
+import 'antd/dist/reset.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <ConfigProvider theme={{
+            token: {}
+        }}>
+            <App />
+        </ConfigProvider>
     </React.StrictMode>,
 )
